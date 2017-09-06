e8dc4a3716548578128c726013ad159f64feb216
Isaac Avery <isaac@webmarketing-inc.com>
2017-09-05 18:07:28 -0700

    Added 'checked_out' field for the sheets

--- database/migrations/2017_07_27_220325_create_sheets_table.php
+++ database/migrations/2017_07_27_220325_create_sheets_table.php
@@ -35,7 +35,7 @@ class CreateSheetsTable extends Migration
             $table->date('date_signed')->nullable();
             $table->integer('signature_count')->nullable();
             $table->boolean('self_signed')->default(false);
-            $table->dateTime('checked_out')->default(0);
+            $table->dateTime('checked_out')->nullable()->default(null);
             $table->integer('user_id')
                 ->references('id')
                 ->on('users');
