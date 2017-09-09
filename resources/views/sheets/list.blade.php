8bcca6dabea54d3365f3554a4292b4f66aadb8f2
Isaac Avery <isaac@webmarketing-inc.com>
2017-09-09 09:12:40 -0700

    WIP on Sheet list.

--- resources/views/sheets/list.blade.php
+++ resources/views/sheets/list.blade.php
@@ -16,23 +16,23 @@
                                     <th>Date Added</th>
                                     <th>Date Signed</th>
                                     <th># Sig</th>
-                                    <th>Flagged</th>
-                                    <th>Completed</th>
-                                    <th>Approved</th>
-                                    <th>Links</th>
+                                    <th>FLAG</th>
+                                    <th>CIR</th>
+                                    <th>SIG</th>
+                                    <th>REV</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @foreach($sheets as $sheet)
                                 <tr>
                                     <td>{{ $sheet->id }}</td>
-                                    <td>{{ $sheet->created_at }}</td>
-                                    <td></td>
-                                    <td></td>
-                                    <td>{!! (!$sheet->flagged_by) ? '<span class="glyphicon glyphicon-flag text-danger"></span>' : '' !!}</td>
-                                    <td>{!! (!$sheet->completed_by) ? '<span class="glyphicon glyphicon-ok text-primary"></span>' : '' !!}</td>
-                                    <td>{!! (!$sheet->approved_by) ? '<span class="glyphicon glyphicon-thumbs-up text-success"></span>' : '' !!}</td>
-                                    <td><a href="/sheets/{{ $sheet->id }}/edit"><span class="glyphicon glyphicon-lg glyphicon-pencil"></span> Edit</a></td>
+                                    <td>{!! date('m/d/y', strtotime($sheet->created_at)) . ' <small><span class="text-muted">(' . $sheet->batch_id . ')</span></small>' !!}</td>
+                                    <td>{{ date('m/d/y', strtotime($sheet->date_signed)) }}</td>
+                                    <td>{{ $sheet->signature_count }}</td>
+                                    <td>{!! ($sheet->flagged_by) ? '<a href="/sheets/' . $sheet->id . '"><span class="glyphicon glyphicon-flag text-danger"></span></a>' : '' !!}</td>
+                                    <td>{!! ($sheet->circulator_completed_by) ? '<span class="glyphicon glyphicon-ok text-primary"></span>' : '' !!}</td>
+                                    <td>{!! ($sheet->signatures_completed_by) ? '<span class="glyphicon glyphicon-ok text-primary"></span>' : '' !!}</td>
+                                    <td>{!! ($sheet->reviewed_by) ? '<span class="glyphicon glyphicon-thumbs-up text-success"></span>' : '' !!}</td>
                                 </tr>
                                 @endforeach
                             </tbody>
