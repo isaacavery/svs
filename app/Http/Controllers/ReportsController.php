7d641add8de32b210255530db6bd8d2be8c5e9cf
Isaac Avery <isaac@webmarketing-inc.com>
2017-10-12 10:31:28 -0700

    Updated the circulator reports to include estimated and actual signature counts

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -32,7 +32,11 @@ class ReportsController extends Controller
     		COALESCE(v.city, c.city) AS city,
     		COALESCE(v.state, c.state) AS state,
     		COALESCE(v.zip_code, c.zip_code) AS zip_code,
-    		zip_plus_four,eff_address_1,eff_address_2,eff_address_3,eff_address_4,eff_city,eff_state,eff_zip_code,eff_zip_plus_four,absentee_type,precinct_name,precinct,split from circulators c LEFT JOIN voters v USING (voter_id) ORDER BY 1');
+    		zip_plus_four,eff_address_1,eff_address_2,eff_address_3,eff_address_4,eff_city,eff_state,eff_zip_code,eff_zip_plus_four,absentee_type,precinct_name,precinct,split, est as estimated_signatures, act as actual_signatures from circulators c 
+            LEFT JOIN voters v USING (voter_id)
+            LEFT JOIN (select sum(signature_count) est, circulator_id FROM sheets GROUP BY circulator_id) e ON (c.id = e.circulator_id)
+            LEFT JOIN (SELECT COUNT(sheets.id) act, circulator_id FROM signers JOIN sheets ON (sheets.id = signers.sheet_id) GROUP BY circulator_id) a ON (c.id = a.circulator_id)
+             ORDER BY 1 ');
     	if(!$circulators)
     		return 'There are no circulators to generate a report on.';
     	$filename = "uploads/circulators.csv";
