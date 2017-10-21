9f68c42e8df4f422b0f29035fecab855d9f63997
Isaac Avery <isaac@webmarketing-inc.com>
2017-10-21 16:16:28 -0700

    Fixed a bug in the Signers Count query in the header.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                         @else
                             <li style="color:#ccc;">Circulators added: {{ Auth::user()->circulators()->count() }}<br>
-                            Signers added: {{ DB::table('signers')->join('sheets','sheets.id','=','signers.sheet_id')->whereNull('sheets.self_signed')->where('signers.user_id',Auth::user()->id)->count() }}</li>
+                            Signers added: {{ DB::table('signers')->join('sheets','sheets.id','=','signers.sheet_id')->where('sheets.self_signed',0)->where('signers.user_id',Auth::user()->id)->count() }}</li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
