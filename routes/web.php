f3387b67b020b54fce8cdc6e235813566ea694ae
Isaac Avery <isaac@webmarketing-inc.com>
2017-10-21 19:05:07 -0700

    Moved the API functionality into it's own controller.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('api/searchSigned','HomeController@searchSigned');
+Route::get('api/searchSigned','APIController@searchSigned');
 
 Auth::routes();
 
